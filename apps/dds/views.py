70ab48be83a48368c9b3d3b65e0c26b2e8f4cfac
r.safargaliev <r.safargaliev@itcode.group>
2025-08-14 01:36:19 +0500

    Добавил обработчик исключений

--- apps/dds/views.py
+++ apps/dds/views.py
@@ -1,27 +1,43 @@
+import logging
+
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from apps.dds import models, serializers
 
+logger = logging.getLogger("webserver")
+
 
 @extend_schema(tags=["Transaction"])
 class Transaction(viewsets.ModelViewSet):
-    queryset = models.Transaction.objects.all()
-    serializer_class = serializers.Transaction
+    try:
+        queryset = models.Transaction.objects.all()
+        serializer_class = serializers.Transaction
+    except Exception:
+        logger.exception("Ошибка при работе с Transaction")
 
 
 @extend_schema(tags=["Type"])
 class Type(viewsets.ModelViewSet):
-    queryset = models.Type.objects.all()
-    serializer_class = serializers.Type
+    try:
+        queryset = models.Type.objects.all()
+        serializer_class = serializers.Type
+    except Exception:
+        logger.exception("Ошибка при работе с Type")
 
 
 @extend_schema(tags=["Category"])
 class Category(viewsets.ModelViewSet):
-    queryset = models.Category.objects.all()
-    serializer_class = serializers.Category
+    try:
+        queryset = models.Category.objects.all()
+        serializer_class = serializers.Category
+    except Exception:
+        logger.exception("Ошибка при работе с Category")
 
 
 @extend_schema(tags=["Subcategory"])
 class Subcategory(viewsets.ModelViewSet):
-    queryset = models.Subcategory.objects.all()
-    serializer_class = serializers.SubCategory
+    try:
+        queryset = models.Subcategory.objects.all()
+        serializer_class = serializers.SubCategory
+    except Exception:
+        logger.exception("Ошибка при работе с Subcategory")
